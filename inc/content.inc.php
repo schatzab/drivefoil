2240c3929ddd2193619b63f23256ffa94a2b540f
Chelsea <me@chelseatown.com>
2016-12-10 13:43:53 -0800

    structure changes part

---
+++ inc/content.inc.php
@@ -0,0 +1,8 @@
+<div class="content">
+	<a href="/"><img src="images/logo.png" alt="Drive Foil Logo" class="logo"></a>
+
+	<!-- Footer -->
+	<?php include("footer.inc.php"); ?>
+</div>
+
+
--- index.php
+++ index.php
@@ -16,11 +16,8 @@
 <!-- Primary Structure -->
 <div class="container">
 	<?php include("inc/nav.inc.php"); ?>
-	<?php include("inc/primary.inc.php"); ?>
+	<?php include("inc/content.inc.php"); ?>
 </div>
-
-<!-- Footer -->
-<?php include("inc/footer.inc.php"); ?>
 
 <!--js-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
