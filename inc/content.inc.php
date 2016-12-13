a1288de9eda6831f74f52967290ded6dc85c81df
Chelsea <me@chelseatown.com>
2016-12-13 13:51:59 -0800

    responsive web changes to sass

--- inc/content.inc.php
+++ inc/content.inc.php
@@ -1,6 +1,9 @@
 <div class="content">
 	<div class="content_first">
 		<a href="/"><img src="images/logo.png" alt="Drive Foil Logo" class="logo"></a>
+
+		<h1>Mathmatically Foiled</h1>
+		<h2>Fatten up your fin and sweeten your ride</h2>
 	</div>
 
 	<!-- Footer -->
