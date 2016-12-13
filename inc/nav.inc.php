e086ae7aee2813cfe0e97328b4438a5f598b90d7
Chelsea <me@chelseatown.com>
2016-12-13 10:19:19 -0800

    sass updates

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,7 +1,7 @@
 <nav class="nav">
-  <div class="nav_1"><a href="/">Fins</a></div>
-  <div class="nav_2"><a href="/">Green</a></div>
-  <div class="nav_3"><a href="/">Quotes</a></div>
-  <div class="nav_4"><a href="/">Buy</a></div>
-  <div class="nav_5"><a href="/">Contact</a></div>
+  <div class="nav_box"><a href="/">Fins</a></div>
+  <div class="nav_box"><a href="/">Green</a></div>
+  <div class="nav_box"><a href="/">Quotes</a></div>
+  <div class="nav_box"><a href="/">Buy</a></div>
+  <div class="nav_box"><a href="/">Contact</a></div>
 </nav>
