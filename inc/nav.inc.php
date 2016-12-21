af7f593f267b6971c1eac24139697d403f75aa1e
Chelsea <me@chelseatown.com>
2016-12-21 13:49:52 -0800

    toggle adjustments

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,7 +1,28 @@
 <nav class="nav">
-  <div class="nav_box"><div class="nav_labels">hide me</div><img src="/images/icon_fin.png"></div>
-  <div class="nav_box"><img src="/images/icon_leaves.png"></div>
-  <div class="nav_box"><img src="/images/icon_quotes.png"></div>
-  <div class="nav_box"><img src="/images/icon_cart.png"></div>
-  <div class="nav_box"><img src="/images/icon_contact.png"></div>
+
+  <div class="nav_box">
+  	<img src="/images/icon_fin.png" class="nav_box_img">
+  	<div class="nav_labels">Drive Foil Fins</div>
+  </div>
+
+    <div class="nav_box">
+  	<img src="/images/icon_leaves.png" class="nav_box_img">
+   	<div class="nav_labels">Environment Friendly</div>
+  </div>
+
+    <div class="nav_box">
+  	<img src="/images/icon_quotes.png" class="nav_box_img">
+   	<div class="nav_labels">Testimonials</div>
+  </div>
+
+    <div class="nav_box">
+  	<img src="/images/icon_cart.png" class="nav_box_img">
+   	<div class="nav_labels">Purchase</div>
+  </div>
+
+    <div class="nav_box">
+  	<img src="/images/icon_contact.png" class="nav_box_img">
+   	<div class="nav_labels">Contact Us</div>
+  </div>
+
 </nav>
